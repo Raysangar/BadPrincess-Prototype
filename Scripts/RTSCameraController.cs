ec7a387f6dd74386ec3358812a35a1797f2d9ed4
Rayco Sánchez García <raycosanchez@gmail.com>
2014-11-25 15:40:32 +0100

    First Commit. RTS Camera Controller added. Single Unit Selector and Controller added.

---
+++ Scripts/RTSCameraController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RTSCameraController : MonoBehaviour
+{
+    private const int ZoomSpeed = 50;
+    private const int ZoomMin = 25;
+    private const int ZoomMax = 100;
+    private const int PanSpeed = 50;
+    private const int PanAngleMin = 50;
+    private const int PanAngleMax = 80;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mouse Buttons and Edge Scrolling Stuff
+        float mousePosX = Input.mousePosition.x;
+        float mousePosY = Input.mousePosition.y;
+        int scrollDistance = 5;
+        float scrollSpeed = 200;
+        float ScrollAmount = scrollSpeed * Time.deltaTime;
+
+        //mouse left
+        if (mousePosX < scrollDistance)
+        {
+            transform.Translate(-ScrollAmount, 0, 0, Space.World);
+        }
+        //mouse right
+        if (mousePosX >= (Screen.width - scrollDistance))
+        {
+            transform.Translate(ScrollAmount, 0, 0, Space.World);
+        }
+        //mouse down
+        if (mousePosY < scrollDistance)
+        {
+            transform.Translate(0, 0, -ScrollAmount, Space.World);
+        }
+        //mouse up
+        if (mousePosY >= (Screen.height - scrollDistance))
+        {
+            transform.Translate(0, 0, ScrollAmount, Space.World); ;
+        }
+
+        //Keyboard controls 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, ScrollAmount, Space.World); ;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -ScrollAmount, Space.World);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-ScrollAmount, 0, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(ScrollAmount, 0, 0, Space.World);
+        }
+    }
+}
---
+++ UnitController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+    private GameObject unit;
+    private Vector3 targetPosition;
+    public int speed = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                targetPosition.x = hitInfo.point.x;
+                targetPosition.y = hitInfo.point.y;
+                targetPosition.z = hitInfo.point.z;
+            }
+        }
+        if (unit != null)
+        {
+            
+                unit.transform.LookAt(new Vector3(targetPosition.x, 0, targetPosition.z));
+                unit.transform.position = Vector3.MoveTowards(unit.transform.position, targetPosition, speed * Time.deltaTime);
+                //unit.transform.Translate(hitInfo.point.x, unit.transform.position.y, hitInfo.point.z);
+        }
+	}
+
+    public void setUnit(GameObject unit)
+    {
+        if (this.unit != null)
+            this.unit.renderer.material.color = Color.white;
+        this.unit = unit;
+        if (this.unit != null)
+        {
+            this.unit.renderer.material.color = Color.blue;
+            targetPosition = unit.transform.position;
+        }
+    }
+}
