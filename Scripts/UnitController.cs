651ce0302e290bd3f86d73bb8cace6c4a8ec0838
Rayco Sánchez García <raycosanchez@gmail.com>
2014-12-03 14:23:37 +0100

    UnitSelector and UnitController new are able to select/control multiple units

--- Scripts/UnitController.cs
+++ Scripts/UnitController.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 public class UnitController : MonoBehaviour {
-    private GameObject unit;
+    private ArrayList units;
     private Vector3 targetPosition;
     public int speed = 10;
 	// Use this for initialization
 	void Start () {
-	
+        units = new ArrayList();
+        targetPosition = -Vector3.one;
 	}
 	
 	// Update is called once per frame
@@ -23,24 +24,35 @@ public class UnitController : MonoBehaviour {
                 targetPosition.z = hitInfo.point.z;
             }
         }
-        if (unit != null)
+        if (units != null && targetPosition != -Vector3.one)
         {
-            
+            foreach (GameObject unit in units)
+            {
                 unit.transform.LookAt(new Vector3(targetPosition.x, 0, targetPosition.z));
                 unit.transform.position = Vector3.MoveTowards(unit.transform.position, targetPosition, speed * Time.deltaTime);
                 //unit.transform.Translate(hitInfo.point.x, unit.transform.position.y, hitInfo.point.z);
+            }
         }
 	}
 
+    public void setUnits(ArrayList units)
+    {
+        foreach (GameObject unit in this.units)
+            unit.renderer.material.color = Color.white;
+        this.units = units;
+        foreach (GameObject unit in this.units)
+            unit.renderer.material.color = Color.blue;
+    }
+
     public void setUnit(GameObject unit)
     {
-        if (this.unit != null)
-            this.unit.renderer.material.color = Color.white;
-        this.unit = unit;
-        if (this.unit != null)
+        foreach (GameObject oldUnit in units)
+            oldUnit.renderer.material.color = Color.white;
+        units = new ArrayList();
+        if (unit != null)
         {
-            this.unit.renderer.material.color = Color.blue;
-            targetPosition = unit.transform.position;
+            unit.renderer.material.color = Color.blue;
+            units.Add(unit);
         }
     }
 }
---
+++ Scripts/UnitSelectorController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitSelectorController : MonoBehaviour {
+    private static Rect selectionRectangle = new Rect(0,0,0,0);
+    private UnitController unitController;
+    private Vector3 startClick = -Vector3.one;
+    private GameObject[] units;
+
+	void Start () {
+        unitController = gameObject.GetComponent<UnitController>();
+        units = GameObject.FindGameObjectsWithTag("Unit");
+	}
+	
+	void Update () {
+        UpdateSelectionRectangle();
+        if (Input.GetButtonUp("Fire1"))
+        {
+            if (selectionRectangle.width == 0 && selectionRectangle.height == 0)
+                shootRay();
+            else
+                unitController.setUnits(GetSelectedUnits());
+        }
+	}
+
+    private void UpdateSelectionRectangle()
+    { 
+        if (Input.GetButtonDown("Fire1"))
+            startClick = Input.mousePosition;
+        if (Input.GetButton("Fire1"))
+            scaleSelectionRectangle();
+        if (Input.GetButtonUp("Fire1"))
+        {
+            transformNegativeScales();
+            startClick = -Vector3.one;
+        }
+    }
+
+    private ArrayList GetSelectedUnits()
+    {
+        ArrayList selectedUnits = new ArrayList();
+        foreach (GameObject unit in units)
+        {
+            if (unit.renderer.isVisible)
+            {
+                Vector3 unitPositionOnCamera = Camera.main.WorldToScreenPoint(unit.transform.position);
+                unitPositionOnCamera.y = transformYAxisFromScreenToWorld(unitPositionOnCamera.y);
+                if (selectionRectangle.Contains(unitPositionOnCamera))
+                    selectedUnits.Add(unit);
+            }
+        }
+        return selectedUnits;
+    }
+
+    private void scaleSelectionRectangle()
+    {
+        selectionRectangle = new Rect(startClick.x, transformYAxisFromScreenToWorld(startClick.y), Input.mousePosition.x - startClick.x,
+            transformYAxisFromScreenToWorld(Input.mousePosition.y) - transformYAxisFromScreenToWorld(startClick.y));
+    }
+
+    private static float transformYAxisFromScreenToWorld(float y)
+    {
+        return Screen.height - y;
+    }
+
+    private void transformNegativeScales()
+    {
+        if (selectionRectangle.width < 0)
+            transformRectangleWidth();
+        if (selectionRectangle.height < 0)
+            transformRectangleHeight();
+    }
+
+    private void transformRectangleWidth()
+    {
+        selectionRectangle.y += selectionRectangle.width;
+        selectionRectangle.width = -selectionRectangle.width;
+    }
+
+    private void transformRectangleHeight()
+    {
+        selectionRectangle.x += selectionRectangle.height;
+        selectionRectangle.height = -selectionRectangle.height;
+    }
+
+    private void shootRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            if (hitInfo.transform.gameObject.layer.Equals(8))
+                unitController.setUnit(hitInfo.transform.gameObject);
+            else
+                unitController.setUnit(null);
+        }
+    }
+}
