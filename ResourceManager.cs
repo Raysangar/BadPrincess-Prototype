2fc43d77854fb79b1fab3006691e3f240cc44fa8
Rayco Sánchez García <raycosanchez@gmail.com>
2014-12-08 13:26:20 +0100

    UI created for resource representation. New ResourceManager script which control UI resource representation

---
+++ ResourceManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResourceManager : MonoBehaviour {
+    private int wood = 0;
+    private int stone = 200;
+    private int iron = 0;
+    public Text ironText;
+    public Text woodText;
+    public Text stoneText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ironText.text = iron.ToString();
+        woodText.text = wood.ToString();
+        stoneText.text = stone.ToString();
+	}
+
+    public void addStone(int amount)
+    {
+        stone += amount;
+    }
+
+    public void addWood(int amount)
+    {
+        wood += amount;
+    }
+
+    public void addIron(int amount)
+    {
+        iron += amount;
+    }
+}
